63b5afdea10bc7abd21f4adc7d3cee268b06b6c3
sakimilo <wonglingyit@gmail.com>
2021-05-06 09:25:43 +0800

    second commit to remove ignored folders

--- bin/clean.sh
+++ bin/clean.sh
@@ -3,4 +3,6 @@
 rm -f logs/*.log
 find . -name __pycache__ | xargs rm -rf
 
-rm -rf notebooks/.ipynb_checkpoints
+rm -rf ../notebooks/.ipynb_checkpoints
+rm -rf ../.ipynb_checkpoints
+rm -rf .pytest_cache
